e4c71ce93f2a83cec62bda9531c0a9e468e35dbd
sabbir53rahman <sabbir53rahman@gmail.com>
2024-07-05 00:24:33 +0600

    dfsg

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,23 +5,23 @@ import { IoMdMail } from "react-icons/io";
 
 function Contact() {
   return (
-    <div className="bg-black min-h-screen">
-      <div className="container mx-auto py-10 px-4">
-        <div className="flex flex-col md:flex-row gap-6">
-          <div className="bg-[#E7FBFF] md:w-1/2 mt-[150px] p-[50px] rounded-[20px] flex flex-col justify-start">
+    <div className="bg-black my_container min-h-screen">
+      <div className=" mx-auto py-10 px-4">
+        <div className="flex flex-col lg:flex-row gap-6">
+          <div className="bg-[#E7FBFF] text-center lg:text-start lg:w-1/2 mt-[150px] p-[50px] rounded-[20px] flex flex-col justify-start">
             <h1 className="text-[48px] lg:text-[65px] text-black mb-8">
               Contact Us
             </h1>
             <div className="pb-[20px]">
-              <div className="flex ">
+              <div className="flex justify-center lg:justify-normal">
               <FaMapMarkerAlt className="text-[25px] text-[#6a6a8e] mt-[5px] mr-2" />
-              <h3 className="text-[25px] text-[#6a6a8e]">Address:</h3>
+              <h3 className="text-[25px]  text-[#6a6a8e]">Address:</h3>
               </div>
               <p className="text-[#6a6a8e] text-[18px] pl-[30px]">1 New Layout, Benin City, Nigeria.</p>
               
             </div>
             <div className="pb-[20px]">
-              <div className="flex ">
+              <div className="flex justify-center lg:justify-normal">
               <IoMdMail className="text-[25px] text-[#6a6a8e] mt-[5px] mr-2" />
               <h3 className="text-[25px] text-[#6a6a8e]">Email:</h3>
               </div>
@@ -35,7 +35,7 @@ function Contact() {
                 </p>
             </div>
             <div>
-              <div className="flex ">
+              <div className="flex justify-center lg:justify-normal">
               <FaGlobeAfrica className="text-[25px] text-[#6a6a8e] mt-[5px] mr-2" />
               <h3 className="text-[25px] text-[#6a6a8e]">Creators Ideology</h3>
               </div>
@@ -43,7 +43,7 @@ function Contact() {
               
             </div>
           </div>
-          <div className="md:w-1/2 md:pt-[150px] pb-[50px] px-4">
+          <div className="lg:w-1/2 md:pt-[150px] pb-[50px] px-4">
             <p className="text-white inline-block rounded py-[5px] px-[10px] border border-gray-500">
               Ready to work with us
             </p>
@@ -133,71 +133,72 @@ function Contact() {
                 className="relative flex justify-center items-center w-full gap-3 button group mt-4 px-4 py-2 border border-white text-white rounded-[6px] overflow-hidden"
               >
                 Send a Message
-                <span className="button-hover absolute group-hover:w-[1100px] group-hover:h-[1000px] group-hover:bottom-[-100%] group-hover:right-[-100%]"></span>
+                <span className="button-hover absolute group-hover:w-[1400px] group-hover:h-[1200px] group-hover:bottom-[-100%] group-hover:right-[-100%]"></span>
               </button>
             </form>
           </div>
         </div>
       </div>
-      <div className="container mx-auto px-[15px] pb-[60px]">
+      <div className=" mx-auto px-[15px] pb-[60px]">
         <h1 className="md:text-[36px] text-[22px] pb-[50px] font-bold text-center text-white">
           Explore Our Work
         </h1>
         <div className="flex flex-col gap-[30px]">
-          <div className="container mx-auto px-[20px] h-[20vh] lg:h-[18vh] flex flex-row items-center justify-between rounded bg-[#441DBC]">
-            <div>
-              <h1 className="text-lg md:text-[32px] font-semibold text-white">
-                R'S koso
-              </h1>
-              <button className="self-start w-auto flex items-center gap-3 button group mt-4  border border-white text-white rounded">
-                See Case Study
-                <HiArrowLongRight className="text-[25px]" />
-                <span className="button-hover group-hover:w-[350px] group-hover:h-[350px] group-hover:bottom-[-100%] group-hover:right-[-100%]"></span>
-              </button>
-            </div>
-            <div className="flex flex-col items-end">
-              <h1 className="text-lg md:text-[32px] font-semibold text-white">
-                20%
-              </h1>
-              <p className=" text-white"> Increase in Brand Awareness</p>
-            </div>
+        <div className="w-full mx-auto px-[20px] h-auto md:h-[18vh] flex flex-col md:flex-row items-center justify-between rounded bg-[#441DBC] py-[20px] md:py-[0] ">
+          <div className="mb-4 md:mb-0">
+            <h1 className="text-lg md:text-[32px] text-center md:text-start font-semibold text-white">
+              R'S koso
+            </h1>
+            <button className="self-start w-auto flex items-center gap-3 button group mt-4 border border-white text-white rounded">
+              See Case Study
+              <HiArrowLongRight className="text-[25px]" />
+              <span className="button-hover group-hover:w-[350px] group-hover:h-[350px] group-hover:bottom-[-100%] group-hover:right-[-100%]"></span>
+            </button>
           </div>
-          <div className="container mx-auto px-[20px] h-[20vh] lg:h-[18vh] flex flex-row items-center justify-between rounded bg-[#441DBC]">
-            <div>
-              <h1 className="text-lg md:text-[32px] font-semibold text-white">
-                Fum
-              </h1>
-              <button className="self-start w-auto flex items-center gap-3 button group mt-4  border border-white text-white rounded">
-                See Case Study
-                <HiArrowLongRight className="text-[25px]" />
-                <span className="button-hover group-hover:w-[350px] group-hover:h-[350px] group-hover:bottom-[-100%] group-hover:right-[-100%]"></span>
-              </button>
-            </div>
-            <div className="flex flex-col items-end">
-              <h1 className="text-lg md:text-[32px] font-semibold text-white">
-                30%
-              </h1>
-              <p className=" text-white"> Increase in Product Desirability</p>
-            </div>
+          <div className="flex flex-col items-center md:items-end">
+            <h1 className="text-lg md:text-[32px] font-semibold text-white">
+              20%
+            </h1>
+            <p className="text-white">Increase in Brand Awareness</p>
           </div>
-          <div className="container mx-auto px-[20px] h-[20vh] lg:h-[18vh] flex flex-row items-center justify-between rounded bg-[#441DBC]">
-            <div>
-              <h1 className="text-lg md:text-[32px] font-semibold text-white">
-                Ekster
-              </h1>
-              <button className="self-start w-auto flex items-center gap-3 button group mt-4  border border-white text-white rounded">
-                See Case Study
-                <HiArrowLongRight className="text-[25px]" />
-                <span className="button-hover group-hover:w-[350px] group-hover:h-[350px] group-hover:bottom-[-100%] group-hover:right-[-100%]"></span>
-              </button>
-            </div>
-            <div className="flex flex-col items-end">
-              <h1 className="text-lg md:text-[32px] font-semibold text-white">
-                40%
-              </h1>
-              <p className=" text-white"> Increase in Brand Reputation</p>
-            </div>
+        </div>
+        <div className="w-full mx-auto px-[20px] h-auto md:h-[18vh] flex flex-col md:flex-row items-center justify-between rounded bg-[#441DBC] py-[20px] md:py-[0]">
+          <div className="mb-4 md:mb-0">
+            <h1 className="text-lg md:text-[32px] text-center md:text-start font-semibold text-white">
+              Fum
+            </h1>
+            <button className="self-start w-auto flex items-center gap-3 button group mt-4 border border-white text-white rounded">
+              See Case Study
+              <HiArrowLongRight className="text-[25px]" />
+              <span className="button-hover group-hover:w-[350px] group-hover:h-[350px] group-hover:bottom-[-100%] group-hover:right-[-100%]"></span>
+            </button>
           </div>
+          <div className="flex flex-col items-center md:items-end">
+            <h1 className="text-lg md:text-[32px] font-semibold text-white">
+              30%
+            </h1>
+            <p className="text-white">Increase in Product Desirability</p>
+          </div>
+        </div>
+
+        <div className="w-full mx-auto px-[20px] h-auto md:h-[18vh] flex flex-col md:flex-row items-center justify-between rounded bg-[#441DBC] py-[20px] md:py-[0]">
+          <div className="mb-4 md:mb-0">
+            <h1 className="text-lg md:text-[32px] text-center md:text-start font-semibold text-white">
+              Ekster
+            </h1>
+            <button className="self-start w-auto flex items-center gap-3 button group mt-4 border border-white text-white rounded">
+              See Case Study
+              <HiArrowLongRight className="text-[25px]" />
+              <span className="button-hover group-hover:w-[350px] group-hover:h-[350px] group-hover:bottom-[-100%] group-hover:right-[-100%]"></span>
+            </button>
+          </div>
+          <div className="flex flex-col items-center md:items-end">
+            <h1 className="text-lg md:text-[32px] font-semibold text-white">
+              40%
+            </h1>
+            <p className="text-white">Increase in Brand Reputation</p>
+          </div>
+        </div>
         </div>
       </div>
     </div>
--- src/components/Study/Study.jsx
+++ src/components/Study/Study.jsx
@@ -3,99 +3,102 @@ import { HiArrowLongRight } from "react-icons/hi2";
 
 function Study() {
   return (
-    <div className="bg-black px-[20px] pb-[60px] pt-[120px]">
+    <div className="bg-black my_container pb-[60px] pt-[120px]">
       <h1 className="md:text-[56px] text-[42px] pb-[50px] font-bold text-center text-white">
         Explore Our Work
       </h1>
       <div className="flex flex-col gap-[30px]">
-        <div className="container mx-auto px-[20px] h-[20vh] lg:h-[18vh] flex flex-row items-center justify-between rounded bg-[#441DBC]">
-          <div>
-            <h1 className="text-lg md:text-[32px] font-semibold text-white">
+        <div className="w-full mx-auto px-[20px] h-auto md:h-[18vh] flex flex-col md:flex-row items-center justify-between rounded bg-[#441DBC] py-[20px] md:py-[0] ">
+          <div className="mb-4 md:mb-0">
+            <h1 className="text-lg md:text-[32px] text-center md:text-start font-semibold text-white">
               R'S koso
             </h1>
-            <button className="self-start w-auto flex items-center gap-3 button group mt-4  border border-white text-white rounded">
+            <button className="self-start w-auto flex items-center gap-3 button group mt-4 border border-white text-white rounded">
               See Case Study
               <HiArrowLongRight className="text-[25px]" />
               <span className="button-hover group-hover:w-[350px] group-hover:h-[350px] group-hover:bottom-[-100%] group-hover:right-[-100%]"></span>
             </button>
           </div>
-          <div className="flex flex-col items-end">
+          <div className="flex flex-col items-center md:items-end">
             <h1 className="text-lg md:text-[32px] font-semibold text-white">
               20%
             </h1>
-            <p className=" text-white"> Increase in Brand Awareness</p>
+            <p className="text-white">Increase in Brand Awareness</p>
           </div>
         </div>
-        <div className="container mx-auto px-[20px] h-[20vh] lg:h-[18vh] flex flex-row items-center justify-between rounded bg-[#441DBC]">
-          <div>
-            <h1 className="text-lg md:text-[32px] font-semibold text-white">
+        <div className="w-full mx-auto px-[20px] h-auto md:h-[18vh] flex flex-col md:flex-row items-center justify-between rounded bg-[#441DBC] py-[20px] md:py-[0]">
+          <div className="mb-4 md:mb-0">
+            <h1 className="text-lg md:text-[32px] text-center md:text-start font-semibold text-white">
               Fum
             </h1>
-            <button className="self-start w-auto flex items-center gap-3 button group mt-4  border border-white text-white rounded">
+            <button className="self-start w-auto flex items-center gap-3 button group mt-4 border border-white text-white rounded">
               See Case Study
               <HiArrowLongRight className="text-[25px]" />
               <span className="button-hover group-hover:w-[350px] group-hover:h-[350px] group-hover:bottom-[-100%] group-hover:right-[-100%]"></span>
             </button>
           </div>
-          <div className="flex flex-col items-end">
+          <div className="flex flex-col items-center md:items-end">
             <h1 className="text-lg md:text-[32px] font-semibold text-white">
               30%
             </h1>
-            <p className=" text-white"> Increase in Product Desirability</p>
+            <p className="text-white">Increase in Product Desirability</p>
           </div>
         </div>
-        <div className="container mx-auto px-[20px] h-[20vh] lg:h-[18vh] flex flex-row items-center justify-between rounded bg-[#441DBC]">
-          <div>
-            <h1 className="text-lg md:text-[32px] font-semibold text-white">
+
+        <div className="w-full mx-auto px-[20px] h-auto md:h-[18vh] flex flex-col md:flex-row items-center justify-between rounded bg-[#441DBC] py-[20px] md:py-[0]">
+          <div className="mb-4 md:mb-0">
+            <h1 className="text-lg md:text-[32px] text-center md:text-start font-semibold text-white">
               Ekster
             </h1>
-            <button className="self-start w-auto flex items-center gap-3 button group mt-4  border border-white text-white rounded">
+            <button className="self-start w-auto flex items-center gap-3 button group mt-4 border border-white text-white rounded">
               See Case Study
               <HiArrowLongRight className="text-[25px]" />
               <span className="button-hover group-hover:w-[350px] group-hover:h-[350px] group-hover:bottom-[-100%] group-hover:right-[-100%]"></span>
             </button>
           </div>
-          <div className="flex flex-col items-end">
+          <div className="flex flex-col items-center md:items-end">
             <h1 className="text-lg md:text-[32px] font-semibold text-white">
               40%
             </h1>
-            <p className=" text-white"> Increase in Brand Reputation</p>
+            <p className="text-white">Increase in Brand Reputation</p>
           </div>
         </div>
-        <div className="container mx-auto px-[20px] h-[20vh] lg:h-[18vh] flex flex-row items-center justify-between rounded bg-[#441DBC]">
-          <div>
-            <h1 className="text-lg md:text-[32px] font-semibold text-white">
+
+        <div className="w-full mx-auto px-[20px] h-auto md:h-[18vh] flex flex-col md:flex-row items-center justify-between rounded bg-[#441DBC] py-[20px] md:py-[0]">
+          <div className="mb-4 md:mb-0">
+            <h1 className="text-lg md:text-[32px] text-center md:text-start font-semibold text-white">
               Scribe
             </h1>
-            <button className="self-start w-auto flex items-center gap-3 button group mt-4  border border-white text-white rounded">
+            <button className="self-start w-auto flex items-center gap-3 button group mt-4 border border-white text-white rounded">
               See Case Study
               <HiArrowLongRight className="text-[25px]" />
               <span className="button-hover group-hover:w-[350px] group-hover:h-[350px] group-hover:bottom-[-100%] group-hover:right-[-100%]"></span>
             </button>
           </div>
-          <div className="flex flex-col items-end">
+          <div className="flex flex-col items-center md:items-end">
             <h1 className="text-lg md:text-[32px] font-semibold text-white">
               30%
             </h1>
-            <p className=" text-white"> CAC Reduction</p>
+            <p className="text-white">CAC Reduction</p>
           </div>
         </div>
-        <div className="container mx-auto px-[20px] h-[20vh] lg:h-[18vh] flex flex-row items-center justify-between rounded bg-[#441DBC]">
-          <div>
-            <h1 className="text-lg md:text-[32px] font-semibold text-white">
+
+        <div className="w-full mx-auto px-[20px] h-auto md:h-[18vh] flex flex-col md:flex-row items-center justify-between rounded bg-[#441DBC] py-[20px] md:py-[0]">
+          <div className="mb-4 md:mb-0">
+            <h1 className="text-lg md:text-[32px] text-center md:text-start font-semibold text-white">
               Growth School
             </h1>
-            <button className="self-start w-auto flex items-center gap-3 button group mt-4  border border-white text-white rounded">
+            <button className="self-start w-auto flex items-center gap-3 button group mt-4 border border-white text-white rounded">
               See Case Study
               <HiArrowLongRight className="text-[25px]" />
               <span className="button-hover group-hover:w-[350px] group-hover:h-[350px] group-hover:bottom-[-100%] group-hover:right-[-100%]"></span>
             </button>
           </div>
-          <div className="flex flex-col items-end">
+          <div className="flex flex-col items-center md:items-end">
             <h1 className="text-lg md:text-[32px] font-semibold text-white">
               3x
             </h1>
-            <p className=" text-white"> ROAS</p>
+            <p className="text-white">ROAS</p>
           </div>
         </div>
       </div>
