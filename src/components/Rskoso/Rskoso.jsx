d0f05477986558f4ff51fabe8e0161408b776fc7
sabbir53rahman <sabbir53rahman@gmail.com>
2024-07-14 13:38:28 +0600

    done

--- src/components/Rskoso/Rskoso.jsx
+++ src/components/Rskoso/Rskoso.jsx
@@ -45,7 +45,7 @@ function Rskoso() {
       <div className="my_container text-white py-[80px]">
         {/* Challenge section */}
         <div className="pb-[50px]">
-          <h4 className="text-[35px] font-semibold pb-[20px]">Challenge:</h4>
+          <h4 className="text-[35px] font-semibold pb-[10px]">Challenge:</h4>
           <p className="text-[18px]">
             R'S Koso, a healthy alternative to coffe, needed to break into a
             saturate markert dominated by caffeine giants. Traditional
@@ -61,7 +61,7 @@ function Rskoso() {
 
         {/* Solution section */}
         <div className="pb-[50px]">
-          <h4 className="text-[35px] w-full font-semibold pb-[20px]">
+          <h4 className="text-[35px] w-full font-semibold pb-[10px]">
             Solution:
           </h4>
           <div className="flex flex-col lg:flex-row gap-[30px] pb-[30px]">
@@ -95,7 +95,7 @@ function Rskoso() {
 
         {/* Key Takeaway section */}
         <div className="pb-[50px]">
-          <h4 className="text-[35px] font-semibold pb-[20px]">Key Takeaway</h4>
+          <h4 className="text-[35px] font-semibold pb-[10px]">Key Takeaway</h4>
           <p className="text-[18px]">
             The campaign exceeded expactations, gamenning a total of{" "}
             <strong>200,000 impressions</strong> and over{" "}
