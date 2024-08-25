53a19c53a7664bf0fce25fb0012235d1624be810
Codemyhobby <alhassandamilola16@gmail.com>
2024-08-25 15:49:30 +0100

    loading

--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useLayoutEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 function Main() {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <div>
         <Header/>
